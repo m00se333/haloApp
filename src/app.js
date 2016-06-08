01f0aa1c80c4fc86a277a8870f9eb290b7490ef2
m00se333 <moosecunningham@gmail.com>
2016-06-08 15:20:09 -0700

    commented code

--- src/app.js
+++ src/app.js
@@ -1,40 +1,53 @@
 "use-strict"
 
 var express = require("express");
+//dependencies
 var request = require("request");
 var app = express();
 var bodyParser = require("body-parser")
-
+// I think this is necessary to parse the JSON data I'm recieving
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-
+//express app serves static files located in the public directory
 app.use(express.static(__dirname + '/public'));
 
+//At the home route (localhost:1117/) send the static file index.html
 app.get("/", function(req, res){
 	res.sendFile("index.html");
 });
 
+//Event Listener when there is a POST request made from public/request.js
 app.post("/search", function(req, res){
+	// In this case the req is the POST request and the request body is the data I sent along with it. Refer to request.js
 	var search = req.body.search;
+	//logs what is searched in the terminal, not the JavaScript console
 	console.log("You searched for: " + search);
+	//what does this do?
 	res.end("yes");
 	
+	//these are the settings/options for the GET request I am going to make when the POST comes in from index.html
 	var options = { method: 'GET',
   		url: 'https://www.haloapi.com/stats/h5/servicerecords/warzone',
+  		//the search variable is still within the scope of the POST listener I did this intentionally because taking information out of these requests is what I'm struggling with
  		qs: { players: search },
   		headers: 
    			{ 'postman-token': '4e734e7b-45fa-e332-c6f6-8100ddb23915',
      			'cache-control': 'no-cache',
+     			// Well you guys all know my dev key now
      			'ocp-apim-subscription-key': 'a9433cb3d47b4f7d9dca856b2c3f1809' } };
 
+     	//request is a magical node package that makes XMLHttpRequests I suppose, similar to $.ajax(url {options})
 		request(options, function (error, response, body) {
 		  if (error) throw new Error(error);
+		  // This is necessary because the body is a string, and JSON.parse turns said string into an object
 		  var body = JSON.parse(body)
+		  // Digging through the body object through some console-logging will produce data based on these keys... to the terminal
 		  console.log("Gamertag: " + body.Results[0].Id)
 		  console.log("Total Kills: " + body.Results[0].Result.WarzoneStat.TotalKills);
 			});
+		// What I want to be able to do is take the variable body and make it available to the rest of my app, this is where I am stuck
 })
 
 
--- src/public/request.js
+++ src/public/request.js
@@ -1,9 +1,14 @@
+//define search button for later use
 var $searchButton = $("#searchButton");
 
+//search Button event handler sends off a POST request
 $searchButton.on("click", function(event){
+	//captute the value of the search field when button is clicked
+	// Type in Lore Eye An Us- that's me and a valid Xbox Live Gamertag
 	var $search = $("#searchField").val();
 	event.preventDefault();
 	
+	//jQuery POST request: url, data, dunno what the function is for
 	$.post("http://localhost:1117/search", {search: $search}, function(data){
 		if (data === "DONE"){
 			console.log("POST worked")
